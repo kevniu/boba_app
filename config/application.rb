2dc8ae4e85bb589c07f394c492c6ea6b59c0f2fe
Kevin Niu <kev.niu@gmail.com>
2016-07-24 18:30:38 -0700

    update

--- config/application.rb
+++ config/application.rb
@@ -23,4 +23,11 @@ module BobaApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
+
+  config.middleware.use Rack::Cors do
+    allow do
+      origins '*'
+      resource '/*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
 end
